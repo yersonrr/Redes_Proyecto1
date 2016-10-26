fb05bf947a8f5ad0503ba6faaba041a95e5f33de
Yerson Roa <yerson132@gmail.com>
2016-10-26 16:19:05 -0400

    Added new printf to socked response.

--- Proyecto2016/cliente.c
+++ Proyecto2016/cliente.c
@@ -7,7 +7,7 @@
 
 #define MAXHOSTNAME 1000
 
-void parse_arguments(int argc, char *argv[], char **p, int *i, char **server, char **o) {
+void parse_arguments(int argc, char *argv[], char **port, int *i, char **server, char **o) {
 	char *bad_syntax_msg = "Sintaxis errada de argumentos del programa. Sintaxis correcta: bsd_cli -d <ip_servidor> -p <puerto_servidor> -c <op> -i <codigo_usuario>\n";
 
 
@@ -20,7 +20,7 @@ void parse_arguments(int argc, char *argv[], char **p, int *i, char **server, ch
 	char *id = NULL;
 	char *s = NULL;
 	char *option = NULL;
-	char *port = NULL;
+	char *p = NULL;
 
 	while ((opt = getopt(argc, argv, "d:c:i:p:")) != -1) {
 
@@ -38,7 +38,7 @@ void parse_arguments(int argc, char *argv[], char **p, int *i, char **server, ch
 		    	//printf("Option=%s\n", option);
 		    	break;
 		    case 'p':
-		    	port = optarg;
+		    	p = optarg;
 		    	//printf("Port=%s\n", port);
 		    	break;
 		    case '?':			    
@@ -68,11 +68,11 @@ void parse_arguments(int argc, char *argv[], char **p, int *i, char **server, ch
 		*o = option;
 	}
 
-	if(atoi(port) == 0){
+	if(atoi(p) == 0){
 		printf("El puerto solo debe contener numeros.\n");
 		exit(0);
 	} else {
-		*p = port;
+		*port = p;
 	}
 
 	if(atoi(id) == 0){
@@ -92,35 +92,36 @@ void parse_arguments(int argc, char *argv[], char **p, int *i, char **server, ch
 
 int main (int argc, char *argv[]) {
 	char buffer[50];
-    int sock, try, c, i, j, a, len, R, C;
+    int sock, try, c, id, j, a, len, R, C;
     struct addrinfo hints, *servinfo;
     char *server = NULL;
-    char *o = NULL;
-    char *p = NULL;
+    char *option = NULL;
+    char *port = NULL;
 
-	parse_arguments(argc, argv, &p, &i, &server, &o);
+	parse_arguments(argc, argv, &port, &id, &server, &option);
 	printf("Server: %s\n", server);
-	printf("Puerto: %s\n", p);
-	printf("Id: %d\n", i);
-	printf("Opcion: %s\n", o);
+	printf("Puerto: %s\n", port);
+	printf("Id: %d\n", id);
+	printf("Opcion: %s\n", option);
 
-    //memset(&hints, 0, sizeof(hints));
-    //hints.ai_family = AF_UNSPEC;
-    //hints.ai_socktype = SOCK_STREAM;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
 
-    /*if (getaddrinfo(argv[1], argv[3], &hints, &servinfo) != 0) {
+    if (getaddrinfo(server, port, &hints, &servinfo) != 0) {
         printf("ERROR: no se pudo resolver el nombre del servidor.\n");
         return -1;
-    }*/
+    }
 
- 	/*while (1) {
+    
+ 	while (1) {
  		sock = socket(AF_INET, SOCK_STREAM, 0);
  		if (connect(sock, servinfo->ai_addr, servinfo->ai_addrlen) != 0) {
  			printf("ERROR: no se pudo conectar con el servidor.\n");
         	return -1;
  		}
 
-	 	sprintf(buffer, "%d %d", r, c);
+	 	sprintf(buffer, "%d %s", id, option);
 	 	try = 3;
 
 	 	while (try--)
@@ -134,37 +135,26 @@ int main (int argc, char *argv[]) {
 	    len = recv(sock, buffer, 50, 0);
 	    switch (buffer[0]) {
 	    	case '0':
-	    		printf("Vagon lleno.\n");
-	    		return -1;
-	    	case '1':
-	    		printf("Puesto ocupado.\nPuestos disponibles:\n ");
-	    		buffer[len] = '\0';
-	    		//printf("%d :: %s\n", strlen(buffer), buffer);
-	    		R = buffer[1]-48;
-	    		C = buffer[2]-48;
-	    		if (R == 0) R = 10;
-	    		//printf("%d %d\n", R,C);
-	            for (i=0; i<R; i++) {
-	                for (j=0; j<C; j++) {
-	                    if ((buffer+3)[i*C+j] != '1') printf("(%d,%d) | ", i+1,j+1);
-	                }
-	            }
-
-	    		printf("\nIngrese numero de fila y columna (<fila> <columna>): ");
-	    		scanf("%d %d", &r, &c);
+	    		printf("Deposito hecho con exito.\n");
 	    		close(sock);
-	    		break;
-	    	case '2':
-	    		printf("Puesto reservado con exito.\n");
 	    		return 0;
+	    	case '1':
+	    		printf("Retiro hecho con exito.\n");
+	    		close(sock);
+	    		return 0;
+	    	case '2':
+	    		printf("No es posible establecer el retiro.\n");
+	    		close(sock);
+	    		return -1;
 	    	case '3':
-	    		printf("Rango errado.\n");
+	    		printf("Ha retirado la cantidad maxima establecida por el cajero\n");
+	    		close(sock);
 	    		return -1;
 	    	default:
 	    		printf("El servidor esta presentando una falla. Recibido: %c.\n", buffer[0]);
+	    		close(sock);
 	    		return -1;
 	    }
-	}*/
-    //close(sock);
+	}
 	return 0;
 }
